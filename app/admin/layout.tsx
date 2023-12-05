11a04b4424cc959ff253da587ec736e92b430f22
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-12-05 23:54:08 +0000

    003

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
-import Aside from "../components/Aside"
 import NavBar from "../components/NavBar"
+import Aside from "../components/Aside"
 
 export default function AdminLayout({
     children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
-import Courses from "./admin/page";
-import Footer from "./admin/layout";
-import Header from "./";
-
-export const revalidate = 0
-export default function Home() {
+export default function Admin(){
   return (
-    <div className="bg-[#3d3d3d]">
-      <div className="max-w-[1440px] m-auto ">
-        <Header />
-        <Courses />
-        <Footer children={undefined} />
+      <div>
+      <h1 className="text-center text-xl">Seja Bem 5464Vindo</h1>
+      
+    
+  
 
       </div>
-    </div>
-
-
+      
   )
 }
