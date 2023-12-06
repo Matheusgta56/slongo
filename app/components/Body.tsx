45f1db95354f820fc12aea831b573e746d511b3f
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-12-06 01:31:36 +0000

    fgh

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -1,12 +1,30 @@
 
 
 export default function Body() {
-    return(
+    return (
         <div className="flex">
             <div className="w-1/4 border-r">
                 <h2>Inscrição</h2>
-                <a className="text-white" href="Listing">Listagem</a>
-               
+                <a href="/Listing">
+                    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2">
+                    Registro
+
+                    </button>
+                </a>
+                <a href="/Listing">
+                    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2">
+                    Listagem
+
+                    </button>
+                </a>
+                <a href="/Listing">
+                    <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2">
+                    Exclusão
+
+                    </button>
+                </a>
+
+
             </div>
 
         </div>
